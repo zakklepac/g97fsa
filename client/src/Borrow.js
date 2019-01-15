0d0601e7db40bbd403114edf404096878397b123
zakklepac <41202649+zakklepac@users.noreply.github.com>
2019-01-14 18:32:40 -0600

    workingo n comps

--- client/src/Borrow.js
+++ client/src/Borrow.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
  
 class Borrow extends Component {
+
+  state ={
+    data: null
+  };
+
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+  this.callBackendAPI()
+    .then(res => this.setState({ data: res.express }))
+    .catch(err => console.log(err));
+}
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/borrow');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+
   render() {
     return (
       <div>
         <h2>Borrow</h2>
-            <p>put cat logic here</p>
+        <p className="App-intro">{this.state.data}</p>
       </div>
     );
   }
+  
 }
  
 export default Borrow;
--- client/src/Lend.js
+++ client/src/Lend.js
@@ -1,11 +1,52 @@
 import React, { Component } from "react";
  
 class Lend extends Component {
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
+  };
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch('/lend');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/lend', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+    this.setState({ responseToPost: body });
+  };
+
   render() {
     return (
       <div>
-        <h2>Lend</h2>
-            <p>put cat form here</p>
+        <h2>Lend a Cat</h2>
+        <p>{this.state.response}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <strong>Post to Server:</strong>
+          </p>
+          <input
+            type="text"
+            value={this.state.post}
+            onChange={e => this.setState({ post: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p>
       </div>
     );
   }
